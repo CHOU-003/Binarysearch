b0249c3d1d0c7affb7c6e73da37321556c07cd96
CHOU-003 <103943138+CHOU-003@users.noreply.github.com>
2023-04-06 08:35:50 +0700

    Add files via upload

---
+++ CTDL&GT_Quick_Sort_2.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+
+void Swap(int *a[50], int x, int y){
+       // Hoan vi
+    int temp = a[x];
+    a[x] = a[y];
+    a[y] = temp;
+}
+
+int partition(int *a[50], int left, int right){
+    // gia tri chot bang gia tri phan tu dau tien
+    int Chot = a[left];
+
+    int count = 0;
+
+    // dem so luong nam ben trai chot
+    for (int i = left+1; i <= right; i++)
+    {
+        if (a[i]<=Chot)
+            count++;
+    }
+    // lay vi tri chot
+    int ChotIndex = left+count;
+
+    //hoan vi
+    Swap(a, left, ChotIndex);
+
+    int i=left;
+    int j=right;
+
+    while (i<ChotIndex && j>ChotIndex)
+    {
+        while (a[i] <= Chot)
+        {
+            i++;
+        }
+        while (a[j] > Chot)
+        {
+            j--;
+        }
+        if (i<ChotIndex && j>ChotIndex)
+        {
+            Swap(a, i, j);
+        }
+        
+        return ChotIndex;
+    }
+    
+
+}
+
+void Sapxepnhanh(int a[50], int left, int right){
+
+    if(right <= left){
+        return;
+    }
+
+    // chia vùng
+    int Chot = partition(a, left, right);
+
+    // Sap xep day ben trai
+    Sapxepnhanh(a, left, Chot-1);
+
+    // sap xep day phai
+    Sapxepnhanh(a, Chot-1, right);
+
+}
+
+int main(){
+    int n;
+    int a[50];
+
+    printf("Nhap vao n: ");
+    scanf("%d", &n);
+
+    printf("Nhap vao day so: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    Sapxepnhanh(a, 0, n-1);
+
+    printf("\n day da sap la: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+    }
+    
+}
